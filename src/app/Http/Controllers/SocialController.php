8fb92207ea0faa41e8008f2d685b77ea54316e91
Th0rn0 <dev@th0rn0.co.uk>
2021-02-20 18:07:30 +0000

    discord login

---
+++ src/app/Http/Controllers/SocialController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Validator;
+use Socialite;
+use Exception;
+use Auth;
+
+class SocialController extends Controller
+{
+    public function discordRedirect()
+    {
+        return Socialite::driver('discord')->redirect();
+    }
+
+    public function loginWithDiscord()
+    {
+        try {
+    
+            $user = Socialite::driver('discord')->user();
+            $isUser = User::where('discord_id', $user->id)->first();
+     
+            if($isUser){
+                Auth::login($isUser);
+                return redirect('/dashboard');
+            }else{
+                $createUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'discord_id' => $user->id,
+                    'password' => encrypt('admin@123')
+                ]);
+    
+                Auth::login($createUser);
+                return redirect('/dashboard');
+            }
+    
+        } catch (Exception $exception) {
+            dd($exception->getMessage());
+        }
+    }
+}
